4d519370d1e6bf6f85377308bff7d82106d4df7f
Hynek Hanke <hanke@brailcom.org>
2002-12-16 01:39:44 +0000

    (set_client_name()): Now affects also history client settings.

--- src/server/set.c
+++ src/server/set.c
@@ -1,4 +1,5 @@
 #include "speechd.h"
+#include "history.h"
 
 int
 set_priority(int fd, int priority){
@@ -28,16 +29,23 @@ set_language(int fd, char *language){
 }
 
 int
-set_client_name(int fd, char *language){
+set_client_name(int fd, char *client_name){
 	GList *gl;
     TFDSetElement *settings;
+	THistoryClient *hclient;
 										  
 	gl = g_list_find_custom(fd_settings, (int*) fd, p_fdset_lc);
 	if (gl == NULL) FATAL("Couldnt find settings for active client, internal error.");
 	settings = gl->data;
 	free(settings->client_name);
-	settings->client_name = (char*) malloc(strlen(language) * sizeof(char) + 1);
-	strcpy(settings->client_name, language);
+	settings->client_name = (char*) malloc(strlen(client_name) * sizeof(char) + 1);
+	strcpy(settings->client_name, client_name);
+
+    gl = g_list_find_custom(history, (int*) fd, p_cli_comp_id);
+    if (gl == NULL) return 0;
+    hclient = gl->data;
+	strcpy(hclient->client_name, client_name);
+	
 	return 1;
 }
 		 
