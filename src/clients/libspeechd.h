40c99840b07f61d0cc8913d09ed408379a95b62a
Hynek Hanke <hanke@brailcom.org>
2003-01-04 22:04:04 +0000

    (spd_stop_fd()) (spd_pause_fd()) (spd_resume_fd(fd)

--- src/clients/libspeechd.h
+++ src/clients/libspeechd.h
@@ -6,8 +6,13 @@ int spd_sayf (int fd, int priority, char *format, ...);
 int spd_stop(int fd);
 int spd_pause(int fd);
 int spd_resume(int fd);
-		
+int spd_stop_fd(int fd, int target);
+int spd_pause_fd(int fd, int target);
+int spd_resume_fd(int fd, int target);
 
 /* internal functions */
 char* send_data(int fd, char *message, int wfr);
 int isanum(char* str);		
+char* get_rec_str(char *record, int pos);
+int get_rec_int(char *record, int pos);
+char* parse_response_data(char *resp, int pos);
