dfc25e694144082a8dbb08d0dde023276be5e2e5
Hynek Hanke <hanke@brailcom.org>
2003-01-04 22:08:36 +0000

    New header file for spd_center.c

---
+++ src/clients/spd_center.h
@@ -0,0 +1,70 @@
+
+/* Gettext support */
+#define _(String) (String)
+
+/* Main data structure defining current user environment */
+typedef struct {
+	char *client;				/* Currently selected client */
+	char *subclient;
+	int client_id;
+	int message_id;
+	int sorted_by;
+	int pos;
+	int mode;
+	int pos_d;
+}TSPDCEnv;
+
+/* Position between the two windows (int pos) */
+#define POS_CMDL 0
+#define POS_MENU 1
+#define POS_HELP 2
+
+/* Position in different menus (int pos_d) */
+#define POSD_ROOT 0
+#define POSD_GLOBAL 1
+#define POSD_SELECT_CLIENT 2
+#define POSD_SELECT_SUBCLIENT 3
+#define POSD_SELECT_MODE 4
+
+#define MODE_HISTORY 0
+#define MODE_SETTINGS 1
+
+/* Sorted by... */
+#define SORT_ALPHABET 0
+#define SORT_TIME 1
+
+/* Default values */
+int SORT_DEFAULT = 0;
+int POS_DEFAULT = 1;
+int POSD_DEFAULT = 0;
+int MODE_DEFAULT = 0;
+
+/* Declare global variables */
+
+TSPDCEnv selected;
+
+/* Declaration of variables defining layout */
+int CMD_L_X, CMD_L_Y;
+int BAR_HELP_X, BAR_HELP_Y, INDENT_HELP;
+int BAR_DIV_X, BAR_DIV_Y;
+int BAR_STAT_X, BAR_STAT_Y, INDENT_STAT;
+int MENU_WIN_X, MENU_WIN_Y, MENU_WIN_L, MENU_WIN_H;
+
+/* Menus and windows */
+WINDOW *_menu_win;
+MENU *menu_m;
+
+/* Prompt */
+char *PROMPT;
+
+/* ??? */
+const int _MENU_CHOSE = 888;
+const int _SWITCH_CMDLINE = 999;
+
+/* Speech Deamon connections */
+int s_main_fd;
+int s_status_fd;
+
+/* Function prototypes */
+void quit(int err_code);
+
