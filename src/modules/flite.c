11873eb242c6a3afa32c52340ebfba3fc5cf75c3
Hynek Hanke <hanke@brailcom.org>
2003-03-12 22:22:40 +0000

    (flite_pause()): Function removed, unnecessary. (flite_resume()): Function removed, unnecessary. (flite_close()): New function.

--- src/modules/flite.c
+++ src/modules/flite.c
@@ -21,10 +21,10 @@
  * the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
  * Boston, MA 02111-1307, USA.
  *
- * $Id: flite.c,v 1.5 2003-02-01 22:16:55 hanke Exp $
+ * $Id: flite.c,v 1.6 2003-03-12 22:22:40 hanke Exp $
  */
 
-#define VERSION "0.0.1"
+#define VERSION "0.0.2"
 
 #include <stdio.h>
 #include <glib.h>
@@ -36,9 +36,8 @@
 
 gint       flite_write      (const gchar *data, gint len, TFDSetElement *);
 gint       flite_stop       (void);
-gint       flite_pause      (void);
-gint       flite_release    (void);
 gint       flite_is_speaking (void);
+gint       flite_close(void);
 
 /* fill the module_info structure with pointers to this modules functions */
 OutputModule modinfo_flite = {
@@ -47,9 +46,8 @@ OutputModule modinfo_flite = {
    NULL, /* filename */
    flite_write,
    flite_stop,
-   flite_pause,
-   flite_release,
-   flite_is_speaking
+   flite_is_speaking,
+   flite_close
 };
 
 /* entry point of this module */
@@ -65,53 +63,35 @@ OutputModule *module_init(void) {
 /* module operations */
 gint flite_write(const gchar *data, gint len, TFDSetElement* set) {
    int i;
-   char comm[BUF_SIZE+256];
    FILE *temp;
 
    printf("flite: write()\n");
 
-   printf("settings: \n");
-   printf("  fd: %d\n", set->fd);
-//   printf("  language: %s\n", set->language);
-
-   for (i=0; i<len-1; i++) {
-      printf("%c",data[i]);
-   }
-   printf("\n");
-//	exit(0);
    data[len-1] = 0;
-
-   sprintf(comm,"flite -t \"%s\" &", data);
-   system(comm);
-   
-//   if((temp = fopen("/tmp/flite_message", "w")) == NULL)
-//      FATAL("Output module flite couldn't open temporary file");
-//   fprintf(temp,"%s\n\r",data);
-//   fclose(temp);
-//   system("flite /tmp/flite_message &");
-   printf("leaving\n\r");
+ 
+   if((temp = fopen("/tmp/flite_message", "w")) == NULL)
+      FATAL("Output module flite couldn't open temporary file");
+   fprintf(temp,"%s\n\r",data);
+   fclose(temp);
+   system("flite /tmp/flite_message &");
+   printf("flite: leaving\n\r");
    return len;
 }
 
 gint flite_stop(void) {
    printf("flite: stop()\n");
    system("killall flite");
-   return 1;
-}
-
-gint flite_pause(void) {
-   printf("flite: pause()\n");
-   return 1;
-}
-
-gint flite_release(void) {
-   printf("flite: release()\n");
-   return 1;
+   return 0;
 }
 
 gint flite_is_speaking(void) {
    int ret;
    ret = system("pgrep flite > /dev/null"); 
    if (ret == 0) return 1;
+   return 0;
+}
+
+gint flite_close(void) {
+   printf("flite: close()\n");
    return 0;
 }
