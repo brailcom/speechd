029ab68879a56001c18790c11b60d3ed4036912d
Tomas Cerha <cerha@brailcom.org>
2003-03-26 08:50:56 +0000

    Initial version.

---
+++ src/python/speechd/_test.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+# Copyright (C) 2003 Brailcom, o.p.s.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+import unittest
+
+import client
+
+class TestSuite(unittest.TestSuite):
+    def add(self, cls, prefix = 'check_'):
+        tests = filter(lambda s: s[:len(prefix)] == prefix, dir(cls))
+        self.addTest(unittest.TestSuite(map(cls, tests)))
+tests = TestSuite()
+
+class Client(unittest.TestCase):
+    def check_it(self):
+        c = client.Client('xxx','yyy')
+        c.close()
+tests.add(Client)
+
+def get_tests():
+    return tests
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='get_tests')
