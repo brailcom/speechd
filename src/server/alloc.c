289aadebf59bbeebf87d3f641aef31ee85eaf8f5
Hynek Hanke <hanke@brailcom.org>
2003-03-29 20:23:46 +0000

    (history_list_new_client): Deleted, using global history now.

--- src/server/alloc.c
+++ src/server/alloc.c
@@ -51,44 +51,24 @@ history_list_new_message(TSpeechDMessage *old)
 	TSpeechDMessage* new = NULL;
 
 	new = (TSpeechDMessage *) spd_malloc(sizeof(TSpeechDMessage));
+
+	/* Copy the content */
+	*new = *old;
 	new->buf = spd_malloc(old->bytes+1);
+	memcpy(new->buf, old->buf, old->bytes);
+	new->buf[old->bytes] = '\0';
+	(TFDSetElement) new->settings = (TFDSetElement) old->settings; 
+
 	new->settings.language = (char*) spd_malloc(strlen(old->settings.language) + 1);
 	new->settings.client_name = (char*) spd_malloc(strlen(old->settings.client_name) + 1);
 	new->settings.output_module = (char*) spd_malloc(strlen(old->settings.output_module) + 1);
 
-	/* Copy the content */
-	*new = *old;
-	memcpy(new->buf, old->buf, old->bytes);
-	(TFDSetElement) new->settings = (TFDSetElement) old->settings; 
 	strcpy(new->settings.language, old->settings.language);
 	strcpy(new->settings.client_name, old->settings.client_name);
 	strcpy(new->settings.output_module, old->settings.output_module);
 
 	return new;
 }
-
-THistoryClient*
-history_list_create_client(int fd)
-{
-	THistoryClient *new;
-	TFDSetElement *settings;
-	GList *gl;
-	
-	settings = (TFDSetElement*) g_hash_table_lookup(fd_settings, &fd);
-	if (settings == NULL) FATAL("Couldn't find appropiate settings for active client."); 	
-
-	new = spd_malloc(sizeof(THistoryClient));
-	new->client_name = spd_malloc(strlen(settings->client_name)+1);
-
-	strcpy(new->client_name, settings->client_name);
-	new->fd = fd;
-	new->uid = settings->uid;
-	new->active = 1;
-	new->messages = NULL;
-
-	return new;
-}
-
 void
 mem_free_message(TSpeechDMessage *msg)
 {
