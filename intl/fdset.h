ea2da7d15892335129a5a3ee0e0058df282e0f4a
Hynek Hanke <hanke@brailcom.org>
2003-04-11 20:22:21 +0000

    (TFDSetElement): New entry char* spelling_table.

--- intl/fdset.h
+++ intl/fdset.h
@@ -21,18 +21,18 @@
  * the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
  * Boston, MA 02111-1307, USA.
  *
- * $Id: fdset.h,v 1.8 2003-04-06 23:24:56 hanke Exp $
+ * $Id: fdset.h,v 1.9 2003-04-11 20:22:21 hanke Exp $
  */
 
 typedef enum {                  /* Prefered type of voice */
-        MALE1 = 0,
-		MALE2 = 1,
-		MALE3 = 2,
-        FEMALE1 = 3,
-        FEMALE2 = 4,
-        FEMALE3 = 5,
-        CHILD_MALE = 6,
-        CHILD_FEMALE = 7
+    MALE1 = 0,
+    MALE2 = 1,
+    MALE3 = 2,
+    FEMALE1 = 3,
+    FEMALE2 = 4,
+    FEMALE3 = 5,
+    CHILD_MALE = 6,
+    CHILD_FEMALE = 7
 }EVoiceType;
 
 typedef enum{
@@ -41,25 +41,26 @@ typedef enum{
 }ESort;
 
 typedef struct{
-		unsigned int uid;		/* Unique ID of the client */
-        int fd;					/* File descriptor the client is on. */
-		int active;				/* Is this client still active on socket or gone?*/
-        int paused;				/* Internal flag, 1 for paused client or 0 for normal. */
-        int priority;			/* Priority between 1 and 3 (1 - highest, 3 - lowest) */
-        int punctuation_mode;	/* Punctuation mode: 0, 1 or 2
-								   		0	-	no punctuation
-										1 	-	all punctuation
-										2	-	only user-selected punctuation */
-        signed int speed; 		/* Speed of voice from <-100;+100>, 0 is the default medium */
-        signed int pitch;		/* Pitch of voice from <-100;+100>, 0 is the default medium */
-        char *client_name;		/* Name of the client. */
-        char *language;         /* Selected language name. (e.g. "english", "czech", "french", ...) */
-        char *output_module;    /* Output module name. (e.g. "festival", "flite", "apollo", ...) */
-        EVoiceType voice_type;  /* see EVoiceType definition above */
-        int spelling;           /* Spelling mode: 0 or 1 (0 - off, 1 - on) */
-        int cap_let_recogn;     /* Capital letters recognition: (0 - off, 1 - on) */
-		unsigned int hist_cur_uid;
-		int hist_cur_pos;
-		ESort hist_sorted;
+    unsigned int uid;		/* Unique ID of the client */
+    int fd;					/* File descriptor the client is on. */
+    int active;				/* Is this client still active on socket or gone?*/
+    int paused;				/* Internal flag, 1 for paused client or 0 for normal. */
+    int priority;			/* Priority between 1 and 3 (1 - highest, 3 - lowest) */
+    int punctuation_mode;	/* Punctuation mode: 0, 1 or 2
+                                   0	-	no punctuation
+                                   1 	-	all punctuation
+                                   2	-	only user-selected punctuation */
+    signed int speed; 		/* Speed of voice from <-100;+100>, 0 is the default medium */
+    signed int pitch;		/* Pitch of voice from <-100;+100>, 0 is the default medium */
+    char *client_name;		/* Name of the client. */
+    char *language;         /* Selected language name. (e.g. "english", "czech", "french", ...) */
+    char *output_module;    /* Output module name. (e.g. "festival", "flite", "apollo", ...) */
+    EVoiceType voice_type;  /* see EVoiceType definition above */
+    int spelling;           /* Spelling mode: 0 or 1 (0 - off, 1 - on) */
+    char* spelling_table;	/* Selected spelling table */
+    int cap_let_recogn;     /* Capital letters recognition: (0 - off, 1 - on) */
+    unsigned int hist_cur_uid;
+    int hist_cur_pos;
+    ESort hist_sorted;
 }TFDSetElement;
 
