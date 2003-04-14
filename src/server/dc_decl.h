b44856f9ff4679aeb2c62756bb5f1d829c83be38
Hynek Hanke <hanke@brailcom.org>
2003-04-14 22:50:38 +0000

    (cb_PunctuationSome): New dotconf callback

--- src/server/dc_decl.h
+++ src/server/dc_decl.h
@@ -21,7 +21,7 @@
  * the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
  * Boston, MA 02111-1307, USA.
  *
- * $Id: dc_decl.h,v 1.8 2003-04-11 20:39:12 hanke Exp $
+ * $Id: dc_decl.h,v 1.9 2003-04-14 22:50:38 hanke Exp $
  */
 
 #include "speechd.h"
@@ -36,6 +36,7 @@ DOTCONF_CB(cb_DefaultPitch);
 DOTCONF_CB(cb_DefaultLanguage);
 DOTCONF_CB(cb_DefaultPriority);
 DOTCONF_CB(cb_DefaultPunctuationMode);
+DOTCONF_CB(cb_PunctuationSome);
 DOTCONF_CB(cb_DefaultClientName);
 DOTCONF_CB(cb_DefaultVoiceType);
 DOTCONF_CB(cb_DefaultSpelling);
@@ -46,56 +47,57 @@ DOTCONF_CB(cb_DefaultCapLetRecognition);
 /* define dotconf configuration options */
 static const configoption_t options[] =
 {
-		   {"LogFile", ARG_STR, cb_LogFile, 0, 0},
-		   {"AddModule", ARG_STR, cb_AddModule, 0, 0},
-		   {"AddTable", ARG_STR, cb_AddTable, 0, 0},
-		   {"DefaultModule", ARG_STR, cb_DefaultModule, 0, 0},
-		   {"DefaultSpeed", ARG_INT, cb_DefaultSpeed, 0, 0},
-		   {"DefaultPitch", ARG_INT, cb_DefaultPitch, 0, 0},
-		   {"DefaultLanguage", ARG_STR, cb_DefaultLanguage, 0, 0},
-		   {"DefaultPriority", ARG_INT, cb_DefaultPriority, 0, 0},
-		   {"DefaultPunctuationMode", ARG_STR, cb_DefaultPunctuationMode, 0, 0},
-		   {"DefaultClientName", ARG_STR, cb_DefaultClientName, 0, 0},
-		   {"DefaultVoiceType", ARG_INT, cb_DefaultVoiceType, 0, 0},
-		   {"DefaultSpelling", ARG_TOGGLE, cb_DefaultSpelling, 0, 0},
-		   {"DefaultSpellingTable", ARG_STR, cb_DefaultSpellingTable, 0, 0},
-		   {"DefaultCapLetRecognition", ARG_TOGGLE, cb_DefaultCapLetRecognition, 0, 0},
-		      /*{"ExampleOption", ARG_STR, cb_example, 0, 0},
-			   *      {"MultiLineRaw", ARG_STR, cb_multiline, 0, 0},
-			   *           {"", ARG_NAME, cb_unknown, 0, 0},
-			   *                {"MoreArgs", ARG_LIST, cb_moreargs, 0, 0},*/
-		      LAST_OPTION
+    {"LogFile", ARG_STR, cb_LogFile, 0, 0},
+    {"AddModule", ARG_STR, cb_AddModule, 0, 0},
+    {"AddTable", ARG_STR, cb_AddTable, 0, 0},
+    {"DefaultModule", ARG_STR, cb_DefaultModule, 0, 0},
+    {"DefaultSpeed", ARG_INT, cb_DefaultSpeed, 0, 0},
+    {"DefaultPitch", ARG_INT, cb_DefaultPitch, 0, 0},
+    {"DefaultLanguage", ARG_STR, cb_DefaultLanguage, 0, 0},
+    {"DefaultPriority", ARG_INT, cb_DefaultPriority, 0, 0},
+    {"DefaultPunctuationMode", ARG_STR, cb_DefaultPunctuationMode, 0, 0},
+    {"PunctuationSome", ARG_STR, cb_PunctuationSome, 0, 0},
+    {"DefaultClientName", ARG_STR, cb_DefaultClientName, 0, 0},
+    {"DefaultVoiceType", ARG_INT, cb_DefaultVoiceType, 0, 0},
+    {"DefaultSpelling", ARG_TOGGLE, cb_DefaultSpelling, 0, 0},
+    {"DefaultSpellingTable", ARG_STR, cb_DefaultSpellingTable, 0, 0},
+    {"DefaultCapLetRecognition", ARG_TOGGLE, cb_DefaultCapLetRecognition, 0, 0},
+    /*{"ExampleOption", ARG_STR, cb_example, 0, 0},
+     *      {"MultiLineRaw", ARG_STR, cb_multiline, 0, 0},
+     *           {"", ARG_NAME, cb_unknown, 0, 0},
+     *                {"MoreArgs", ARG_LIST, cb_moreargs, 0, 0},*/
+    LAST_OPTION
 };
 
 DOTCONF_CB(cb_LogFile)
 {
-	assert(cmd->data.str != NULL);
-	if (!strncmp(cmd->data.str,"stdout",6)){
-		logfile = (FILE*) malloc(sizeof(FILE*));
-		logfile = stdout;
-		return NULL;
-	}
-	if (!strncmp(cmd->data.str,"stderr",6)){
-		logfile = (FILE*) malloc(sizeof(FILE*));
-		logfile = stderr;
-		return NULL;
-	}
-	logfile = fopen(cmd->data.str, "w");
-	if (logfile == NULL){
-		printf("Error: can't open logging file! Using stdout.\n");
-		logfile = (FILE*) malloc(sizeof(FILE*));
-		logfile = stdout;
-	}
-	MSG(2,"Speech Deamon Logging to file %s", cmd->data.str);
-	return NULL;
+    assert(cmd->data.str != NULL);
+    if (!strncmp(cmd->data.str,"stdout",6)){
+        logfile = (FILE*) malloc(sizeof(FILE*));
+        logfile = stdout;
+        return NULL;
+    }
+    if (!strncmp(cmd->data.str,"stderr",6)){
+        logfile = (FILE*) malloc(sizeof(FILE*));
+        logfile = stderr;
+        return NULL;
+    }
+    logfile = fopen(cmd->data.str, "w");
+    if (logfile == NULL){
+        printf("Error: can't open logging file! Using stdout.\n");
+        logfile = (FILE*) malloc(sizeof(FILE*));
+        logfile = stdout;
+    }
+    MSG(2,"Speech Deamon Logging to file %s", cmd->data.str);
+    return NULL;
 }
 
 DOTCONF_CB(cb_AddModule)
 {
-	OutputModule *om;
-	om = load_output_module(cmd->data.str);
-	g_hash_table_insert(output_modules, om->name, om);
-	return NULL;
+    OutputModule *om;
+    om = load_output_module(cmd->data.str);
+    g_hash_table_insert(output_modules, om->name, om);
+    return NULL;
 }
 
 DOTCONF_CB(cb_AddTable)
@@ -178,71 +180,82 @@ DOTCONF_CB(cb_AddTable)
 
 DOTCONF_CB(cb_DefaultModule)
 {
-	GlobalFDSet.output_module = malloc(sizeof(char) * strlen(cmd->data.str) + 1);
-	strcpy(GlobalFDSet.output_module,cmd->data.str);
-	return NULL;
+    GlobalFDSet.output_module = malloc(sizeof(char) * strlen(cmd->data.str) + 1);
+    strcpy(GlobalFDSet.output_module,cmd->data.str);
+    return NULL;
 }
 
 DOTCONF_CB(cb_DefaultSpeed)
 {
-	GlobalFDSet.speed = cmd->data.value;
-	return NULL;
+    GlobalFDSet.speed = cmd->data.value;
+    return NULL;
 }
 
 DOTCONF_CB(cb_DefaultPitch)
 {
-	GlobalFDSet.pitch = cmd->data.value;
-	return NULL;
+    GlobalFDSet.pitch = cmd->data.value;
+    return NULL;
 }
 
 DOTCONF_CB(cb_DefaultLanguage)
 {
-	GlobalFDSet.language = malloc(sizeof(char) * strlen(cmd->data.str) + 1);
-	strcpy(GlobalFDSet.language,cmd->data.str);
-	return NULL;
+    GlobalFDSet.language = malloc(sizeof(char) * strlen(cmd->data.str) + 1);
+    strcpy(GlobalFDSet.language,cmd->data.str);
+    return NULL;
 }
 
 DOTCONF_CB(cb_DefaultPriority)
 {
-	GlobalFDSet.priority = cmd->data.value;
-	return NULL;
+    GlobalFDSet.priority = cmd->data.value;
+    return NULL;
 }
 
 DOTCONF_CB(cb_DefaultPunctuationMode)
 {
-	GlobalFDSet.punctuation_mode = cmd->data.value;
-	return NULL;
+    assert(cmd->data.str != NULL);
+    if(!strcmp(cmd->data.str, "all")) GlobalFDSet.punctuation_mode = 1;
+    else if(!strcmp(cmd->data.str, "none")) GlobalFDSet.punctuation_mode = 0;
+    else if(!strcmp(cmd->data.str, "some")) GlobalFDSet.punctuation_mode = 2;
+    return NULL;
+}
+
+DOTCONF_CB(cb_PunctuationSome)
+{
+    assert(cmd->data.str != NULL);
+    GlobalFDSet.punctuation_some = (char*) spd_malloc(sizeof(char) * (strlen(cmd->data.str) + 1));
+    strcpy(GlobalFDSet.punctuation_some, cmd->data.str);
+    return NULL;
 }
 
 DOTCONF_CB(cb_DefaultClientName)
 {
-	GlobalFDSet.client_name = malloc(sizeof(char) * strlen(cmd->data.str) + 1);
-	strcpy(GlobalFDSet.client_name,cmd->data.str);
-	return NULL;
+    GlobalFDSet.client_name = malloc(sizeof(char) * (strlen(cmd->data.str) + 1));
+    strcpy(GlobalFDSet.client_name,cmd->data.str);
+    return NULL;
 }
 
 DOTCONF_CB(cb_DefaultVoiceType)
 {
-	GlobalFDSet.voice_type = (EVoiceType)cmd->data.value;
-	return NULL;
+    GlobalFDSet.voice_type = (EVoiceType)cmd->data.value;
+    return NULL;
 }
 
 DOTCONF_CB(cb_DefaultSpelling)
 {
-	GlobalFDSet.spelling = cmd->data.value;
-	return NULL;
+    GlobalFDSet.spelling = cmd->data.value;
+    return NULL;
 }
 
 DOTCONF_CB(cb_DefaultSpellingTable)
 {
-	GlobalFDSet.spelling_table = (char*) spd_malloc((strlen(cmd->data.str) + 1) * sizeof(char));
-	strcpy(GlobalFDSet.spelling_table,cmd->data.str);
-	return NULL;
+    GlobalFDSet.spelling_table = (char*) spd_malloc((strlen(cmd->data.str) + 1) * sizeof(char));
+    strcpy(GlobalFDSet.spelling_table,cmd->data.str);
+    return NULL;
 }
 
 DOTCONF_CB(cb_DefaultCapLetRecognition)
 {
-	GlobalFDSet.cap_let_recogn = cmd->data.value;
-	return NULL;
+    GlobalFDSet.cap_let_recogn = cmd->data.value;
+    return NULL;
 }
 
