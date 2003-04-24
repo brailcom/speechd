b1e7aa3a8870e9b417c184065514c28e2c1c594f
Hynek Hanke <hanke@brailcom.org>
2003-04-24 19:31:08 +0000

    speed->rate. New prototypes.

--- src/server/set.h
+++ src/server/set.h
@@ -19,7 +19,7 @@
  * the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
  * Boston, MA 02111-1307, USA.
  *
- * $Id: set.h,v 1.5 2003-04-15 10:08:59 pdm Exp $
+ * $Id: set.h,v 1.6 2003-04-24 19:31:08 hanke Exp $
  */
 
 #ifndef ALLOC_H
@@ -28,19 +28,33 @@
 #include "speechd.h"
 #include "history.h"
 
-int set_priority(int fd, int priority);
-int set_language(int fd, char *language);
-int set_rate(int fd, int rate);
-int set_pitch(int fd, int pitch);
-int set_punct_mode(int fd, int punct);
-int set_cap_let_recog(int fd, int recog);
-int set_spelling(int fd, int spelling);
+int set_priority_uid(int uid, int priority);
+int set_language_uid(int uid, char *language);
+int set_rate_uid(int uid, int rate);
+int set_pitch_uid(int uid, int pitch);
+int set_punct_mode_uid(int uid, int punct);
+int set_cap_let_recog_uid(int uid, int recog);
+int set_spelling_uid(int uid, int spelling);
+
+int set_priority_self(int fd, int priority);
+int set_language_self(int fd, char *language);
+int set_rate_self(int fd, int rate);
+int set_pitch_self(int fd, int pitch);
+int set_punct_mode_self(int fd, int punct);
+int set_cap_let_recog_self(int fd, int recog);
+int set_spelling_self(int fd, int spelling);
+
+int set_priority_all(int priority);
+int set_language_all(char *language);
+int set_rate_all(int rate);
+int set_pitch_all(int pitch);
+int set_punct_mode_all(int punct);
+int set_cap_let_recog_all(int recog);
+int set_spelling_all(int spelling);
 
 TFDSetElement* default_fd_set(void);
 		
+void set_param_int(int* parameter, int value);
+void set_param_str(char* parameter, char* value);
 
 #endif		
-		
-		
-		
-		
