a55f372d51f25818d91b28df11f525c601c9b9e0
Hynek Hanke <hanke@brailcom.org>
2003-04-28 02:04:36 +0000

    New files, audio ouput.

---
+++ src/audio/spd_audio.c
@@ -0,0 +1,78 @@
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <glib.h>
+#include <pthread.h>
+#include <flite/flite.h>
+#include <signal.h>
+
+/* Voice */
+extern cst_voice *register_cmu_us_kal();
+cst_voice *spd_audio_flite_voice;
+cst_audiodev *spd_audio_device = NULL;
+
+int
+spd_audio_play_wave(cst_wave *w)
+{
+    int i, r, n;
+
+    for (i=0; i < w->num_samples; i += r/2){
+        if (w->num_samples > i+CST_AUDIOBUFFSIZE){
+            n = CST_AUDIOBUFFSIZE;
+        }else{
+            n = w->num_samples-i;
+        }
+
+        r = audio_write(spd_audio_device, &w->samples[i], n*2);
+        if (r <= 0){
+            cst_errmsg("Audio output: failed to write %d samples\n",n);
+            break;
+        }
+    }
+
+    return 0;
+}
+
+int
+spd_audio_open(cst_wave *w)
+{
+    if ((spd_audio_device = audio_open(w->sample_rate, w->num_channels,
+                         CST_AUDIO_LINEAR16)) == NULL){
+        return CST_ERROR_FORMAT;
+    }
+    return 0;
+}
+
+void
+spd_audio_close()
+{
+    audio_close(spd_audio_device);
+}
+
+/*
+int
+main()
+{
+    cst_wave *wave1, *wave2, *wave3;
+    printf("hello...\n");
+    flite_init();
+    flite_voice = register_cmu_us_kal();
+    if (flite_voice == NULL)
+        printf("Couldn't register the basic kal voice.\n"); 
+
+    wave1 = flite_text_to_wave("c", flite_voice);
+    wave2 = flite_text_to_wave("b", flite_voice);
+    wave3 = flite_text_to_wave("a", flite_voice);
+
+    spd_audio_open(wave1);
+
+    spd_audio_play(wave1);
+    spd_audio_play(wave2);
+    spd_audio_play(wave3);
+
+    spd_audio_close();
+
+}	
+*/
---
+++ src/audio/spd_audio.h
@@ -0,0 +1,4 @@
+
+int spd_audio_play_wave(cst_wave *w);
+int spd_audio_open(cst_wave *w);
+int spd_audio_close();
