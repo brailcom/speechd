edf26dba764de36bac482c514acb6f1c92e7b201
Hynek Hanke <hanke@brailcom.org>
2003-06-05 16:16:36 +0000

    (queue_message): Priorities changed.

--- src/server/server.c
+++ src/server/server.c
@@ -19,7 +19,7 @@
   * the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
   * Boston, MA 02111-1307, USA.
   *
-  * $Id: server.c,v 1.45 2003-06-01 21:24:55 hanke Exp $
+  * $Id: server.c,v 1.46 2003-06-05 16:16:36 hanke Exp $
   */
 
 #include "speechd.h"
@@ -88,11 +88,22 @@ queue_message(TSpeechDMessage *new, int fd, int history_flag, EMessageType type,
     /* Put the element new to queue according to it's priority. */
     switch(settings->priority){
     case 1: MessageQueue->p1 = g_list_append(MessageQueue->p1, new); 
+        alarm(0);
         break;
     case 2: MessageQueue->p2 = g_list_append(MessageQueue->p2, new);
+        alarm(0);
         break;
-    case 3: MessageQueue->p3 = g_list_append(MessageQueue->p3, new);
+    case 3: MessageQueue->p3 = g_list_append(MessageQueue->p3, new);        
+        alarm(0);
         break;
+    case 4: MessageQueue->p4 = g_list_append(MessageQueue->p4, new);        
+        alarm(0);
+        break;
+    case 5: MessageQueue->p5 = g_list_append(MessageQueue->p5, new);        
+        last_p5_message = (TSpeechDMessage*) history_list_new_message(new);
+        alarm(2);
+        break;
+
     default: FATAL("Nonexistent priority given");
     }
     sem_post(sem_messages_waiting);
