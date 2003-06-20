175a3ca2d3d5b97da5428f4d7d73a64502155d55
Hynek Hanke <hanke@brailcom.org>
2003-06-20 00:44:19 +0000

    Prototypes.

--- src/server/parse.h
+++ src/server/parse.h
@@ -2,18 +2,18 @@
 #ifndef PARSE_H
  #define PARSE_H
 
-char* parse(char *buf, int bytes, int fd);
+char* parse(const char* buf, const int bytes, const int fd);
 
-char* parse_history(char *buf, int bytes, int fd);
-char* parse_set(char *buf, int bytes, int fd);
-char* parse_stop(char *buf, int bytes, int fd);
-char* parse_cancel(char *buf, int bytes, int fd);
-char* parse_pause(char *buf, int bytes, int fd);
-char* parse_resume(char *buf, int bytes, int fd);
-char* parse_snd_icon(char *buf, int bytes, int fd);
-char* parse_char(char *buf, int bytes, int fd);
-char* parse_key(char* buf, int bytes, int fd);
-char* parse_list(char* buf, int bytes, int fd);
-char* parse_help(char* buf, int bytes, int fd);
+char* parse_history(const char* buf, const int bytes, const int fd);
+char* parse_set(const char* buf, const int bytes, const int fd);
+char* parse_stop(const char* buf, const int bytes, const int fd);
+char* parse_cancel(const char* buf, const int bytes, const int fd);
+char* parse_pause(const char* buf, const int bytes, const int fd);
+char* parse_resume(const char* buf, const int bytes, const int fd);
+char* parse_snd_icon(const char* buf, const int bytes, const int fd);
+char* parse_char(const char* buf, const int bytes, const int fd);
+char* parse_key(const char* buf, const int bytes, const int fd);
+char* parse_list(const char* buf, const int bytes, const int fd);
+char* parse_help(const char* buf, const int bytes, const int fd);
 
 #endif
