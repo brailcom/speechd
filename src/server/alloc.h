f455e84b11b4371e40e2fe972131fbfd4a959565
Hynek Hanke <hanke@brailcom.org>
2003-06-23 05:09:17 +0000

    (spd_module_free): Added prototype.

--- src/server/alloc.h
+++ src/server/alloc.h
@@ -1,19 +1,17 @@
 
+#include "speechd.h"
+
 #ifndef ALLOC_H
  #define ALLOC_H
 
-#include "speechd.h"
-
-void* spd_malloc(size_t bytes);
+void* spd_malloc(int bytes);
 void spd_free(void *data);
 
 char* spd_strdup(char* string);
 
 TSpeechDQueue* speechd_queue_alloc();					
 
-void* fdset_list_alloc_element(void* element);
-
-TSpeechDMessage* history_list_new_message(TSpeechDMessage *old);
+TSpeechDMessage* spd_message_copy(TSpeechDMessage *old);
 
 void mem_free_message(TSpeechDMessage *msg);
 
