7bbe5bf374d092726f7c8f5a28f36aa4f372d76d
Hynek Hanke <hanke@brailcom.org>
2003-07-21 07:34:19 +0000

    New prototypes included.

--- src/audio/spd_audio.h
+++ src/audio/spd_audio.h
@@ -1,6 +1,15 @@
 
+#ifndef SPD_AUDIO
+#define SPD_AUDIO
+
 #include <flite/flite.h>
 
-int spd_audio_play_wave(cst_wave *w);
-int spd_audio_open(cst_wave *w);
-int spd_audio_close();
+int spd_audio_play_wave(const cst_wave *w);
+int spd_audio_open(const cst_wave *w);
+void spd_audio_close();
+
+int spd_audio_play_file(const char* filename);
+int spd_audio_play_file_ogg(const char* filename);
+int spd_audio_play_file_wav(const char* filename);
+
+#endif /* SPD_AUDIO */
