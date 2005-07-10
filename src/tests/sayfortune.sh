f4c153ed80577408cbd519e4158927b3154a744b
Hynek Hanke <hanke@brailcom.org>
2005-07-10 17:00:52 +0000

    *** empty log message ***

--- src/tests/sayfortune.sh
+++ src/tests/sayfortune.sh
@@ -4,6 +4,6 @@
 #    e.g.:  ./sayfortune.sh
 #           ./sayfortune.sh literature 0 -20
 
-printf "!SET SELF PRIORITY TEXT\n!SET SELF RATE $2\n!SET SELF PITCH $3\n!SPEAK \n `fortune $1` \n.\n" > fortune-speechd.tmp
+printf "!SET SELF PRIORITY TEXT\n!SET SELF RATE $2\n!SET SELF PITCH $3\n!SPEAK \n `fortune $1` \n.\n S3" > fortune-speechd.tmp
 ./run_test fortune-speechd.tmp
 rm fortune-speechd.tmp
