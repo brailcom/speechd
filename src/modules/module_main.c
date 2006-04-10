54ebd21d060b46541c5ed249147114a1046194a0
Hynek Hanke <hanke@brailcom.org>
2006-04-10 17:20:40 +0000

    Relicensed to GNU Lesser General Public License

--- src/modules/module_main.c
+++ src/modules/module_main.c
@@ -1,24 +1,24 @@
 /*
  * module_main.c - One way of doing main() in output modules.
  * 
- * Copyright (C) 2001, 2002, 2003 Brailcom, o.p.s.
+ * Copyright (C) 2001, 2002, 2003, 2006 Brailcom, o.p.s.
  *
- * This is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2, or (at your option)
- * any later version.
+ * This is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1, or (at your option) any later
+ * version.
  *
  * This software is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this package; see the file COPYING.  If not, write to
- * the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
- * Boston, MA 02111-1307, USA.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this package; see the file COPYING.  If not, write to the Free
+ * Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+ * 02111-1307, USA.
  *
- * $Id: module_main.c,v 1.8 2005-10-12 15:59:21 hanke Exp $
+ * $Id: module_main.c,v 1.9 2006-04-10 17:20:40 hanke Exp $
  */
 
 /* So that gcc doesn't comply */
--- src/modules/module_utils_addvoice.c
+++ src/modules/module_utils_addvoice.c
@@ -1,24 +1,24 @@
 /*
  * module_utils_addvoice.c - Functionality for the DotConf AddVoice feature
  * 
- * Copyright (C) 2001, 2002, 2003 Brailcom, o.p.s.
+ * Copyright (C) 2001, 2002, 2003, 2006 Brailcom, o.p.s.
  *
- * This is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2, or (at your option)
- * any later version.
+ * This is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2, or (at your option) any later
+ * version.
  *
  * This software is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this package; see the file COPYING.  If not, write to
- * the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
- * Boston, MA 02111-1307, USA.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this package; see the file COPYING.  If not, write to the Free
+ * Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+ * 02111-1307, USA.
  *
- * $Id: module_utils_addvoice.c,v 1.2 2003-10-12 23:28:09 hanke Exp $
+ * $Id: module_utils_addvoice.c,v 1.3 2006-04-10 17:20:50 hanke Exp $
  */
 
 #include <glib.h>
