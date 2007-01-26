299d4ed19ffead98a58630a76da97ff46cca8f6d
Tomas Cerha <cerha@brailcom.org>
2007-01-26 16:26:12 +0000

    (SSIPClientTest.check_callbacks): New test. (SSIPClientTest.check_notification): Temporarily commented out.

--- src/python/speechd/_test.py
+++ src/python/speechd/_test.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# Copyright (C) 2003, 2006 Brailcom, o.p.s.
+# Copyright (C) 2003, 2006, 2007 Brailcom, o.p.s.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public Licensex1 as published by
@@ -35,59 +35,83 @@ class SSIPClientTest(unittest.TestCase):
         c.set_rate(30)
         return c
 
-    def check_escapes(self):
+    def _check_escapes(self):
         c = self._client()
-        c.speak("Testing data escapes:")
-        c.set_punctuation(PunctuationMode.ALL)
-        c.speak(".")
-        c.speak("Marker at the end.\r\n.\r\n")
-        c.speak(".\r\nMarker at the beginning.")
+        try:
+            c.speak("Testing data escapes:")
+            c.set_punctuation(PunctuationMode.ALL)
+            c.speak(".")
+            c.speak("Marker at the end.\r\n.\r\n")
+            c.speak(".\r\nMarker at the beginning.")
+        finally:
+            c.close()
         
-    def check_voice_properties(self):
+    def _check_voice_properties(self):
         c = self._client()
-        c.speak("Testing voice properties:")
-        c.set_pitch(-100)
-        c.speak("I am fat Billy")
-        c.set_pitch(100)
-        c.speak("I am slim Willy")
-        c.set_pitch(0)
-        c.set_rate(100)
-        c.speak("I am quick Dick.")
-        c.set_rate(-80)
-        c.speak("I am slow Joe.")
-        c.set_rate(0)
-        c.set_pitch(100)
-        c.set_volume(-50)
-        c.speak("I am quiet Mariette.")
-        c.set_volume(100)
-        c.speak("I am noisy Daisy.")
+        try:
+            c.speak("Testing voice properties:")
+            c.set_pitch(-100)
+            c.speak("I am fat Billy")
+            c.set_pitch(100)
+            c.speak("I am slim Willy")
+            c.set_pitch(0)
+            c.set_rate(100)
+            c.speak("I am quick Dick.")
+            c.set_rate(-80)
+            c.speak("I am slow Joe.")
+            c.set_rate(0)
+            c.set_pitch(100)
+            c.set_volume(-50)
+            c.speak("I am quiet Mariette.")
+            c.set_volume(100)
+            c.speak("I am noisy Daisy.")
+        finally:
+            c.close()
 
-    def check_other_commands(self):
+    def _check_other_commands(self):
+        try:
+            c = self._client()
+            c.speak("Testing other commands:")
+            c.char("a")
+            c.key("shift_b")
+            c.sound_icon("empty")
+        finally:
+            c.close()
+        
+    def check_callbacks(self):
         c = self._client()
-        c.speak("Testing other commands:")
-        c.char("a")
-        c.key("shift_b")
-        c.sound_icon("empty")
+        finished = []
+        def callback(type, context):
+            print "**", type, context
+            if type in (CallbackType.CANCEL, CallbackType.END):
+                finished.append(context)
+        try:
+            c.speak("Message 1", callback=lambda type: callback(type, 'msg1'))
+            c.speak("Message 2", callback=lambda type: callback(type, 'msg2'))
+            time.sleep(2)
+            print "= CANCEL ="
+            c.cancel(Scope.ALL)
+            assert 'msg1' in finished and 'msg2' in finished, finished
+        finally:
+            c.close()
 
-    def check_notification(self):
+#    def check_notification(self):
+#         def my_callback(msg_id, client_id, ctype, index_mark=None):            
+#             print "  Callback received on message " + str(msg_id) + \
+#                   " from client " + str(client_id) \
+#                   + ": " + ctype
+#             if index_mark:
+#                 print "    Index mark name: " + index_mark
 
-        def my_callback(msg_id, client_id, ctype, index_mark=None):            
-            print "  Callback received on message " + str(msg_id) + \
-                  " from client " + str(client_id) \
-                  + ": " + ctype
-            if index_mark:
-                print "    Index mark name: " + index_mark
-
-        print "Starting callback test"
-        c = self._client()
-        print "  Cancel all previous messages"
-        c.cancel(Scope.ALL)
-        c.set_notification(my_callback, (CallbackType.BEGIN, CallbackType.END))
-        c.speak("""Informationabout the beginning and the end of this message
-        should appear on the screen""")
-        print "  Waiting for the speech to start and finish in 10 seconds"
-        time.sleep(10)
-        print "End of callback test"
+#         c = self._client()
+#         print "  Cancel all previous messages"
+#         c.cancel(Scope.ALL)
+#         c.set_notification(my_callback, (CallbackType.BEGIN, CallbackType.END))
+#         c.speak("""Informationabout the beginning and the end of this message
+#         should appear on the screen""")
+#         print "  Waiting for the speech to start and finish in 10 seconds"
+#         time.sleep(10)
+#         print "End of callback test"
     
 
 tests.add(SSIPClientTest)
