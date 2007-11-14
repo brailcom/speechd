9e6f8fe1da76a020b93b0fdf7af54c4a78ef6654
Gilles Casse <gcasse@oralux.org>
2007-11-14 22:52:55 +0000

    fixes: rate changes, missing userdata

--- src/audio/pulse.c
+++ src/audio/pulse.c
@@ -20,13 +20,13 @@
  * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
  * 02110-1301, USA.
  *
- * $Id: pulse.c,v 1.1 2007-11-11 21:59:01 gcasse Exp $
+ * $Id: pulse.c,v 1.2 2007-11-14 22:52:55 gcasse Exp $
  */
 
 
 /* debug */
 
-//#define DEBUG_PULSE
+/* #define DEBUG_PULSE */
 #include <stdio.h>
 #include <stdarg.h>
 
@@ -221,7 +221,7 @@ _subscribe_cb(struct pa_context *c, enum pa_subscription_event_type t, uint32_t 
        t != (PA_SUBSCRIPTION_EVENT_SINK_INPUT|PA_SUBSCRIPTION_EVENT_NEW)))
     return;
 
-  if (!(o = pa_context_get_sink_input_info(c, index, _info_cb, NULL))) {
+  if (!(o = pa_context_get_sink_input_info(c, index, _info_cb, userdata))) {
     ERR("pa_context_get_sink_input_info() failed: %s", pa_strerror(pa_context_errno(c)));
     return;
   }
@@ -323,7 +323,7 @@ _stream_request_cb(pa_stream *s, size_t length, void *userdata) {
 
 static void 
 _stream_latency_update_cb(pa_stream *s, void *userdata) {
-  //  ENTER(__FUNCTION__);
+/*   ENTER(__FUNCTION__); */
   assert(s);
   AudioID *id = (AudioID *)userdata;
   if(!id) {
@@ -357,7 +357,6 @@ _volume_time_cb(pa_mainloop_api *api, pa_time_event *e, const struct timeval *tv
   api->time_free(id->pulse_volume_time_event);
   id->pulse_volume_time_event = NULL;
 }
-
 
 static int 
 _pulse_free(AudioID *id, size_t* length) {
@@ -456,42 +455,6 @@ _pulse_playing(AudioID *id, const pa_timing_info *the_timing_info)
 
   return r;
 }
-
-
-// static void pulse_flush(int time) {
-//   ENTER(__FUNCTION__);
-//     pa_operation *o = NULL;
-//     int success = 0;
-
-//     CHECK_CONNECTED(id,);
-
-//     pa_threaded_mainloop_lock(id->pulse_mainloop);
-//     CHECK_DEAD_GOTO(id,fail, 1);
-
-//     if (!(o = pa_stream_flush(id->pulse_stream, _stream_success_cb, &success))) {
-//         ERR("pa_stream_flush() failed: %s", pa_strerror(pa_context_errno(id->pulse_context)));
-//         goto fail;
-//     }
-    
-//     while (pa_operation_get_state(o) != PA_OPERATION_DONE) {
-//         CHECK_DEAD_GOTO(id,fail, 1);
-//         pa_threaded_mainloop_wait(id->pulse_mainloop);
-//     }
-
-//     if (!success)
-//         ERR("pa_stream_flush() failed: %s", pa_strerror(pa_context_errno(id->pulse_context)));
-    
-//     written = (uint64_t) (((double) time * pa_bytes_per_second(pa_stream_get_sample_spec(id->pulse_stream))) / 1000);
-//     id->pulse_just_flushed = 1;
-//     id->pulse_time_offset_msec = time;
-    
-// fail:
-//     if (o)
-//         pa_operation_unref(o);
-    
-//     pa_threaded_mainloop_unlock(id->pulse_mainloop);
-// }
-
 
 static int
 _pulse_write(AudioID *id, void* ptr, int length) 
@@ -621,9 +584,207 @@ _pulse_close(AudioID *id)
   SHOW_TIME("_pulse_close (ret)");
 }
 
+static int
+_pulse_get_sample(const AudioTrack* track, pa_sample_spec* ss)
+{
+  ENTER(__FUNCTION__);
+
+  assert(track && ss);
+
+  /* Choose the correct format */
+  if (track->bits == 16){
+    ss->format = PA_SAMPLE_S16LE;
+  }else if (track->bits == 8){
+    /* TBD: PA_SAMPLE_ALAW, PA_SAMPLE_ULAW ? */
+    ss->format = PA_SAMPLE_U8; 
+  }else{
+    ERR("Unsupported sound data format, track.bits = %d\n", track->bits);
+    return -1;
+  }
+
+  ss->rate = track->sample_rate;
+  ss->channels = track->num_channels;
+
+  SHOW("Setting sample spec to format=%s, rate=%i, count=%i\n", 
+       pa_sample_format_to_string(ss->format),
+       ss->rate, 
+       ss->channels);
+
+  if (!pa_sample_spec_valid(ss)) {      
+    ERR("Sample spec not valid!\n","");
+    return -1;
+  }
+
+  SHOW("Sample spec valid\n","");
+  return 0;
+}
 
 static int 
-_pulse_open(AudioID *id, AudioTrack track) 
+_pulse_open(AudioID *id, pa_sample_spec* ss) 
+{
+  ENTER(__FUNCTION__);
+  
+  pa_operation *o = NULL;
+
+  assert(id && ss);
+
+  assert(!id->pulse_mainloop);
+  assert(!id->pulse_context);
+  assert(!id->pulse_stream);
+  assert(!id->pulse_connected);
+
+  /*     if (!id->pulse_volume_valid) { */
+  pa_cvolume_reset(&id->pulse_volume, ss->channels);
+  id->pulse_volume_valid = 1;
+  /*     } else if (id->pulse_volume.channels != ss.channels) */
+  /*         pa_cvolume_set(&id->pulse_volume, ss.channels, pa_cvolume_avg(&id->pulse_volume)); */
+
+  SHOW_TIME("pa_threaded_mainloop_new (call)");
+  if (!(id->pulse_mainloop = pa_threaded_mainloop_new())) {
+    ERR("Failed to allocate main loop","");
+    goto fail;
+  }
+
+  pa_threaded_mainloop_lock(id->pulse_mainloop);
+
+  SHOW_TIME("pa_context_new (call)");
+  if (!(id->pulse_context = pa_context_new(pa_threaded_mainloop_get_api(id->pulse_mainloop), "speech-dispatcher"))) {
+    ERR("Failed to allocate context","");
+    goto unlock_and_fail;
+  }
+
+  pa_context_set_state_callback(id->pulse_context, _context_state_cb, id);
+  pa_context_set_subscribe_callback(id->pulse_context, _subscribe_cb, id);
+
+  SHOW_TIME("pa_context_connect (call)");
+  if (pa_context_connect(id->pulse_context, id->pulse_server, (pa_context_flags_t)0, NULL) < 0) {
+    ERR("Failed to connect to server: %s", pa_strerror(pa_context_errno(id->pulse_context)));
+    goto unlock_and_fail;
+  }
+
+  SHOW_TIME("pa_threaded_mainloop_start (call)");
+  if (pa_threaded_mainloop_start(id->pulse_mainloop) < 0) {
+    ERR("Failed to start main loop","");
+    goto unlock_and_fail;
+  }
+
+  /* Wait until the context is ready */
+  SHOW_TIME("pa_threaded_mainloop_wait");
+  pa_threaded_mainloop_wait(id->pulse_mainloop);
+
+  if (pa_context_get_state(id->pulse_context) != PA_CONTEXT_READY) {
+    ERR("Failed to connect to server: %s", pa_strerror(pa_context_errno(id->pulse_context)));
+    goto unlock_and_fail;
+  }
+
+  SHOW_TIME("pa_stream_new");
+  if (!(id->pulse_stream = pa_stream_new(id->pulse_context, "unknown", ss, NULL))) {
+    ERR("Failed to create stream: %s", pa_strerror(pa_context_errno(id->pulse_context)));
+    goto unlock_and_fail;
+  }
+
+  pa_stream_set_state_callback(id->pulse_stream, _stream_state_cb, id);
+  pa_stream_set_write_callback(id->pulse_stream, _stream_request_cb, id);
+  pa_stream_set_latency_update_callback(id->pulse_stream, _stream_latency_update_cb, id);
+
+  pa_buffer_attr a_attr;
+  a_attr.maxlength = id->pulse_max_length;
+  a_attr.tlength = id->pulse_target_length;
+  a_attr.prebuf = id->pulse_pre_buffering;
+  a_attr.minreq = id->pulse_min_request;
+  a_attr.fragsize = 882;
+
+  SHOW("attr: maxlength=%i, tlength=%i, prebuf=%i, minreq=%i, fragsize=%i\n",
+       a_attr.maxlength,
+       a_attr.tlength,
+       a_attr.prebuf,
+       a_attr.minreq,
+       a_attr.fragsize);
+
+
+  SHOW_TIME("pa_connect_playback");
+  if (pa_stream_connect_playback(id->pulse_stream, NULL, &a_attr, (pa_stream_flags_t)(PA_STREAM_INTERPOLATE_TIMING|PA_STREAM_AUTO_TIMING_UPDATE), &id->pulse_volume, NULL) < 0) {
+    ERR("Failed to connect stream: %s", pa_strerror(pa_context_errno(id->pulse_context)));
+    goto unlock_and_fail;
+  }
+
+  /* Wait until the stream is ready */
+  SHOW_TIME("pa_threaded_mainloop_wait");
+  pa_threaded_mainloop_wait(id->pulse_mainloop);
+
+  if (pa_stream_get_state(id->pulse_stream) != PA_STREAM_READY) {
+    ERR("Failed to connect stream: %s", pa_strerror(pa_context_errno(id->pulse_context)));
+    goto unlock_and_fail;
+  }
+
+  /* Now subscribe to events */
+  SHOW_TIME("pa_context_subscribe");
+  if (!(o = pa_context_subscribe(id->pulse_context, PA_SUBSCRIPTION_MASK_SINK_INPUT, _context_success_cb, id))) {
+    ERR("pa_context_subscribe() failed: %s", pa_strerror(pa_context_errno(id->pulse_context)));
+    goto unlock_and_fail;
+  }
+    
+  id->pulse_success = 0;
+  SHOW_TIME("pa_threaded_mainloop_wait");
+  while (pa_operation_get_state(o) != PA_OPERATION_DONE) {
+    CHECK_DEAD_GOTO(id,fail, 1);
+    pa_threaded_mainloop_wait(id->pulse_mainloop);
+  }
+
+  if (id->pulse_success == 0) {
+    ERR("pa_context_subscribe() failed: %s", pa_strerror(pa_context_errno(id->pulse_context)));
+    goto unlock_and_fail;
+  }
+
+  pa_operation_unref(o);
+
+  /* Now request the initial stream info */
+  if (!(o = pa_context_get_sink_input_info(id->pulse_context, pa_stream_get_index(id->pulse_stream), _info_cb, id))) {
+    ERR("pa_context_get_sink_input_info() failed: %s", pa_strerror(pa_context_errno(id->pulse_context)));
+    goto unlock_and_fail;
+  }
+    
+  SHOW_TIME("pa_threaded_mainloop_wait 2");
+  while (pa_operation_get_state(o) != PA_OPERATION_DONE) {
+    CHECK_DEAD_GOTO(id, fail, 1);
+    pa_threaded_mainloop_wait(id->pulse_mainloop);
+  }
+
+  /*     if (!id->pulse_volume_valid) { */
+  /*         SHOW("pa_context_get_sink_input_info() failed: %s", pa_strerror(pa_context_errno(context))); */
+  /*         goto unlock_and_fail; */
+  /*     } */
+
+  id->pulse_do_trigger = 0;
+  id->pulse_time_offset_msec = 0;
+  id->pulse_just_flushed = 0;
+  id->pulse_connected = 1;
+  id->pulse_volume_time_event = NULL;
+    
+  pa_threaded_mainloop_unlock(id->pulse_mainloop);
+
+  SHOW("_pulse_open (ret true)\n","");
+
+  return 0;
+
+ unlock_and_fail:
+
+  if (o)
+    pa_operation_unref(o);
+    
+  pa_threaded_mainloop_unlock(id->pulse_mainloop);
+    
+ fail:
+
+  _pulse_close(id);
+
+  SHOW("_pulse_open (ret false)\n","");
+    
+  return -1;
+}
+
+static int 
+_pulse_set_sample(AudioID *id, AudioTrack track) 
 {
   ENTER(__FUNCTION__);
   pa_sample_spec ss;
@@ -631,16 +792,17 @@ _pulse_open(AudioID *id, AudioTrack track)
 
   assert(id);
 
-  assert(!id->pulse_mainloop);
-  assert(!id->pulse_context);
-  assert(!id->pulse_stream);
-  assert(!id->pulse_connected);
+/*   assert(!id->pulse_mainloop); */
+/*   assert(!id->pulse_context); */
+/*   assert(!id->pulse_stream); */
+/*   assert(!id->pulse_connected); */
 
   /* Choose the correct format */
   if (track.bits == 16){
     ss.format = PA_SAMPLE_S16LE;
   }else if (track.bits == 8){
-    ss.format = PA_SAMPLE_U8; // TBD: PA_SAMPLE_ALAW, PA_SAMPLE_ULAW ?
+    /* TBD: PA_SAMPLE_ALAW, PA_SAMPLE_ULAW ? */
+    ss.format = PA_SAMPLE_U8; 
   }else{
     ERR("Unsupported sound data format, track.bits = %d\n", track.bits);
     return -1;
@@ -667,11 +829,11 @@ _pulse_open(AudioID *id, AudioTrack track)
   /*     } else if (id->pulse_volume.channels != ss.channels) */
   /*         pa_cvolume_set(&id->pulse_volume, ss.channels, pa_cvolume_avg(&id->pulse_volume)); */
 
-  SHOW_TIME("pa_threaded_mainloop_new (call)");
-  if (!(id->pulse_mainloop = pa_threaded_mainloop_new())) {
-    ERR("Failed to allocate main loop","");
-    goto fail;
-  }
+/*   SHOW_TIME("pa_threaded_mainloop_new (call)"); */
+/*   if (!(id->pulse_mainloop = pa_threaded_mainloop_new())) { */
+/*     ERR("Failed to allocate main loop",""); */
+/*     goto fail; */
+/*   } */
 
   pa_threaded_mainloop_lock(id->pulse_mainloop);
 
@@ -846,7 +1008,7 @@ pulse_open(AudioID *id, void **pars)
   id->pulse_context = NULL;
   id->pulse_stream = NULL;
   id->pulse_mainloop = NULL;
-  //  id->pulse_volume = 0;
+  /* id->pulse_volume = 0; */
   id->pulse_volume_valid = 0;
   id->pulse_do_trigger = 0;
   id->pulse_time_offset_msec = 0;
@@ -885,6 +1047,7 @@ pulse_play(AudioID *id, AudioTrack track)
   int ret;
   unsigned int sr;
   unsigned int a_total_free_mem;
+  pa_sample_spec ss;
 
   if (id == NULL){
     ERR("Invalid device passed to %s()\n",__FUNCTION__);
@@ -900,12 +1063,27 @@ pulse_play(AudioID *id, AudioTrack track)
     return 0;
   }
 
+  if (_pulse_get_sample(&track, &ss) == -1) {
+    ERR("Erroneous track!\n","");
+    return 0;
+  }
+
+  if (id->pulse_mainloop) {
+    /*  if the sample spec changes, the stream will be rebuilt. */
+    const pa_sample_spec *ss2 = pa_stream_get_sample_spec (id->pulse_stream);
+
+    if (!pa_sample_spec_equal (&ss, ss2)) {
+      _pulse_close(id);
+    }
+  }
+
   if (!id->pulse_mainloop) {
-    ret = _pulse_open(id, track);
+    ret = _pulse_open(id, &ss);
     if (ret != 0) {
       return ret;
     }
   }
+
 
   SHOW("Checking buffer size\n","");
   if (track.bits == 16){
@@ -940,7 +1118,7 @@ pulse_play(AudioID *id, AudioTrack track)
       num_bytes = 0;
     }
     else if (a_total_free_mem > 500) {
-      // 500: threshold for avoiding to many calls to pulse_write
+      /* 500: threshold for avoiding to many calls to pulse_write */
       SHOW("a_total_free_mem(%d) < num_bytes(%d)\n", a_total_free_mem, num_bytes);
       ret = _pulse_write(id, output_samples, a_total_free_mem);
       num_bytes -= a_total_free_mem;
