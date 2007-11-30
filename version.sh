111e9093997c250711c48f5d511230b27240dd99
Hynek Hanke <hanke@brailcom.org>
2007-11-30 11:12:43 +0000

    *** empty log message ***

--- version.sh
+++ version.sh
@@ -2,4 +2,4 @@
 # don't put any spaces around the version-number
 # (look at the top of configure.in to see why)
 # if you change just rerun configure, no need to run autogen.sh
-VERSION=0.6.5rc1
+VERSION=0.6.5
