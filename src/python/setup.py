cd64aeb80305c5f764a382d013c2402a42009b30
Hynek Hanke <hanke@brailcom.org>
2008-07-09 07:55:54 +0000

    (speechd_config) New module.

--- src/python/setup.py
+++ src/python/setup.py
@@ -4,3 +4,9 @@ setup(name='speechd',
       version='0.3',
       packages=['speechd'],
       )
+
+setup(name='speechd_config',
+      version='0.0',
+      packages=['speechd_config'],
+      scripts=['speechd_config/spd-conf']
+      )
