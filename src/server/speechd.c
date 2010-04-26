992f3700bad4de4535d8157b7646e155ed85358e
Hynek Hanke <hanke@brailcom.org>
2010-04-26 10:55:11 +0200

    Correct path for mkdir on ~/.speech-dispatcher

--- src/server/speechd.c
+++ src/server/speechd.c
@@ -885,7 +885,7 @@ main(int argc, char *argv[])
 	/* Setup a ~/.speechd-dispatcher/ directory or create a new one */
 	SpeechdOptions.home_speechd_dir = g_strdup_printf("%s/.speech-dispatcher", user_home_dir);
 	MSG(4, "Home dir found, trying to find %s", SpeechdOptions.home_speechd_dir);
-	g_mkdir(g_path_get_dirname(SpeechdOptions.home_speechd_dir), S_IRWXU);
+	g_mkdir(SpeechdOptions.home_speechd_dir, S_IRWXU);
 	MSG(4, "Using home directory: %s for configuration, pidfile and logging", SpeechdOptions.home_speechd_dir);
 	g_free(user_home_dir);
 
