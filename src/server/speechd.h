de2c285e02e4305eb15017bc8d9e58ef7c3a273d
Hynek Hanke <hanke@brailcom.org>
2010-05-05 14:23:36 +0200

    Improved comments.

--- src/server/speechd.h
+++ src/server/speechd.h
@@ -53,12 +53,13 @@
 #include <sys/sem.h>
 
 /* Definition of semun needed for semaphore manipulation */
-/* TODO: this needs a better check, possibly including _POSIX_C_SOURCE
- * and friends*/
+/* TODO: This fixes compilation for Mac OS X but might not be a correct
+   solution for other platforms. A better check is needed, possibly including
+   _POSIX_C_SOURCE and friends*/
 #if (defined(__GNU_LIBRARY__) && !defined(_SEM_SEMUN_UNDEFINED)) || defined(__APPLE__)
-/* union semun is defined by including <sys/sem.h> */
+ /* union semun is defined by including <sys/sem.h> */
 #else
-/* according to X/OPEN we have to define it ourselves */
+ /* according to X/OPEN we have to define it ourselves */
 union semun {
     int val;                    /* value for SETVAL */
     struct semid_ds *buf;       /* buffer for IPC_STAT, IPC_SET */
