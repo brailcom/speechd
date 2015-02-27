e9d4236095bb4b1ee2936d3e92757b4e031ae2cd
Luke Yelavich <themuso@themuso.com>
2015-02-28 08:31:11 +1100

    API - Change license to LGPL 2.1
    By request from the Qt folks, and thanks to Brailcom for giving their
    approval for this change.
    
    (cherry picked from commit 514c8fc4ec3857b66e0cfab071ea5c2be0f94f29)

--- src/api/c/libspeechd.h
+++ src/api/c/libspeechd.h
@@ -4,16 +4,16 @@
  * Copyright (C) 2001, 2002, 2003, 2004 Brailcom, o.p.s.
  *
  * This is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2, or (at your option)
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1, or (at your option)
  * any later version.
  *
  * This software is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
+ * Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
+ * You should have received a copy of the GNU Lesser General Public License
  * along with this package; see the file COPYING.  If not, write to
  * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
  * Boston, MA 02110-1301, USA.
