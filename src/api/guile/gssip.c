514c8fc4ec3857b66e0cfab071ea5c2be0f94f29
Luke Yelavich <themuso@themuso.com>
2015-02-28 08:31:11 +1100

    API - Change license to LGPL 2.1
    By request from the Qt folks, and thanks to Brailcom for giving their
    approval for this change.

--- src/api/guile/gssip.c
+++ src/api/guile/gssip.c
@@ -7,16 +7,16 @@
    COPYRIGHT NOTICE
 
    This program is free software; you can redistribute it and/or modify it
-   under the terms of the GNU General Public License as published by the Free
-   Software Foundation; either version 2, or (at your option) any later
+   under the terms of the GNU Lesser General Public License as published by the Free
+   Software Foundation; either version 2.1, or (at your option) any later
    version.
 
    This program is distributed in the hope that it will be useful, but
    WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
-   or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+   or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
    for more details.
 
-   You should have received a copy of the GNU General Public License
+   You should have received a copy of the GNU Lesser General Public License
    along with this program; if not, write to the Free Software
    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA.
 */
