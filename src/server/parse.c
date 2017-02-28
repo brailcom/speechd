3dc57ce7b45a2cf0d7959dd3c6264afb2cfa28eb
Luke Yelavich <themuso@themuso.com>
2017-02-28 14:09:46 +1100

    Revert "server: Improve code responsible for sending the synthesis voice"
    This reverts commit 287ab775d612fdc81793f1e11aad5cd4c0d4f599.
    
    There is the potential for accessing the array out of bounds, which would
    cause a crash.

--- src/server/parse.c
+++ src/server/parse.c
@@ -464,14 +464,15 @@ char *parse_set(const char *buf, const int bytes, const int fd,
 		int i;
 
 		split_command = g_strsplit(buf, " ", 0);
-		synthesis_voice = g_strdup(split_command[3]);
+		for (i = 0; split_command[i] != NULL; i++) {
+			tmp = NULL;
 
-		if (synthesis_voice != NULL) {
-			for (i = 4; split_command[i] != NULL; i++) {
-				tmp = NULL;
-
+			if (i == 3) {
+				synthesis_voice = g_strdup(split_command[i]);
+			} else if (i > 3) {
 				if (g_str_has_suffix(split_command[i], "\n")) {
 					tmp = g_strndup(split_command[i], strlen(split_command[i]) - 2);
+					g_free(split_command[i]);
 					split_command[i] = tmp;
 					tmp = NULL;
 				}
@@ -480,9 +481,12 @@ char *parse_set(const char *buf, const int bytes, const int fd,
 				g_free(synthesis_voice);
 				synthesis_voice = tmp;
 			}
-
-			g_strfreev(split_command);
 		}
+
+		g_strfreev(split_command);
+
+		if (i < 3)
+			synthesis_voice = NULL;
 
 		SSIP_SET_COMMAND(synthesis_voice);
 		g_free(synthesis_voice);
