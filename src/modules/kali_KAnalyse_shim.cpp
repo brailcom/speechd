1a026a691d2ba97b3078d033abd2b8f4fad8ee6d
Samuel Thibault <samuel.thibault@ens-lyon.org>
2018-11-02 16:28:33 +0100

    kali: Add a shim
    to be able to build the Kali module without the Kali SDK.

---
+++ src/modules/kali_KAnalyse_shim.cpp
@@ -0,0 +1,36 @@
+/*
+ * kali_KAnalyse_shim.c - Shim for Kali
+ * to be able to build the Kali module without the Kali SDK.
+ *
+ * Copyright (C) 2018 Hypra
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this package; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#include <kali/Kali/kali.h>
+#include <assert.h>
+
+extern "C" {
+
+bool initAnalyse(void) {
+	assert(0);
+}
+
+bool quitteAnalyse(void) {
+	assert(0);
+}
+
+}
