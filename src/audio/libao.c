5fab8f334133791a172e5fd259dba076bd5db84f
Samuel Thibault <samuel.thibault@ens-lyon.org>
2018-11-09 00:12:32 +0100

    Fix typo

--- src/audio/libao.c
+++ src/audio/libao.c
@@ -5,7 +5,7 @@
  * Date:  2009-12-15
  *
  * This is free software; you can redistribute it and/or modify it under the
- * terms of the GNU Leser General Public License as published by the Free
+ * terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation; either version 2.1, or (at your option) any later
  * version.
  *
