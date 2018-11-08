57afcac21137dba49a6e89542e0a214cc8e3975a
Samuel Thibault <samuel.thibault@ens-lyon.org>
2018-11-09 00:12:32 +0100

    Fix typo

--- src/audio/oss.c
+++ src/audio/oss.c
@@ -5,7 +5,7 @@
  * Copyright (C) 2004,2006 Brailcom, o.p.s.
  *
  * This is free software; you can redistribute it and/or modify it under the
- * terms of the GNU Leser General Public License as published by the Free
+ * terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation; either version 2.1, or (at your option) any later
  * version.
  *
