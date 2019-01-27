f166ac9472b5dc20794ea4894fe28cc75ccc1b07
Boris Dušek <dusek@brailcom.org>
2019-01-27 14:36:09 +0100

    Add option to choose a later version of (L)GPL
    This helps resolve possible incompatibility with software licensed
    under later (L)GPL versions.
    
    Approved by Mr Jan Buchal, director of BRAILCOM,o.p.s.

--- src/api/guile/gssip.h
+++ src/api/guile/gssip.h
@@ -8,7 +8,8 @@
 
    This program is free software; you can redistribute it and/or modify it
    under the terms of the GNU Lesser General Public License as published by the Free
-   Software Foundation, version 2.1 of the License.
+   Software Foundation; either version 2.1, or (at your option) any later
+   version.
 
    This program is distributed in the hope that it will be useful, but
    WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
--- src/tests/spd_cancel_long_message.c
+++ src/tests/spd_cancel_long_message.c
@@ -4,8 +4,9 @@
  * Copyright (C) 2010 Brailcom, o.p.s.
 *
 * This program is free software; you can redistribute it and/or modify
-* it under the terms of the GNU General Public License version 2 as
-* published by the Free Software Foundation.
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2, or (at your option)
+* any later version.
 *
 * This program is distributed in the hope that it will be useful,
 * but WITHOUT ANY WARRANTY; without even the implied warranty of
