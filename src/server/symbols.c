e6715925310b1c47a73caefc993f8cdd3695ec88
Samuel Thibault <samuel.thibault@ens-lyon.org>
2019-11-29 02:39:52 +0100

    Fix format

--- src/server/symbols.c
+++ src/server/symbols.c
@@ -597,7 +597,7 @@ static gpointer speech_symbols_new(const gchar *locale)
 
 	for (node = symbols_files; node; node = node->next) {
 		path = g_build_filename(LOCALE_DATA, locale, node->data, NULL);
-		MSG2(5, "symbols", "Trying to load %s for '%s' from '%s'", node->data, locale, path);
+		MSG2(5, "symbols", "Trying to load %s for '%s' from '%s'", (char*) node->data, locale, path);
 		if (speech_symbols_load(ss, path, TRUE) >= 0) {
 			MSG2(5, "symbols", "Successful");
 			/* At least some symbols could be loaded */
