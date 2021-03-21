d964d0773a5adb885154183c23637e82166ecbf0
Samuel Thibault <samuel.thibault@ens-lyon.org>
2021-03-21 22:40:35 +0100

    kali: Use server-side audio rendering and sync call

--- src/modules/kali.cpp
+++ src/modules/kali.cpp
@@ -25,17 +25,12 @@
 #include <config.h>
 #endif
 
-#include <stdio.h>
-#include <string.h>
-#include <unistd.h>
 #include <kali/Kali/kali.h>
-
-#include <semaphore.h>
-#include "spd_audio.h"
 
 #include <speechd_types.h>
 
 #include "module_utils.h"
+
 #define MODULE_NAME     "kali"
 #define MODULE_VERSION  "0.0"
 #define DEBUG_MODULE 1
@@ -44,11 +39,7 @@ DECLARE_DEBUG();
 /* Thread and process control */
 static int kali_speaking = 0;
 
-static pthread_t kali_speak_thread;
-static sem_t kali_semaphore;
-
-static char *kali_message;
-static SPDMessageType kali_message_type;
+static char *buf;
 
 static int kali_position = 0;
 static int kali_pause_requested = 0;
@@ -64,9 +55,8 @@ static void kali_set_punctuation_mode(SPDPunctuation punct);
 static void kali_set_voice(char *voice);
 
 static SPDVoice **kali_get_voices();
-static void *_kali_speak(void *);
 
-int kali_stop = 0;
+static int kali_stop = 0;
 
 MOD_OPTION_1_INT(KaliMaxChunkLength);
 MOD_OPTION_1_STR(KaliDelimiters);
@@ -95,21 +85,11 @@ int module_load(void)
 	return 0;
 }
 
-#define ABORT(msg) g_string_append(info, msg); \
-	DBG("FATAL ERROR:", info->str); \
-	*status_info = info->str; \
-	g_string_free(info, 0); \
-	return -1;
-
 int module_init(char **status_info)
 {
-	int ret;
-	GString *info;
-
 	DBG("Module init");
 
-	*status_info = NULL;
-	info = g_string_new("");
+	module_audio_set_server();
 
 	/* Init kali and register a new voice */
 	initGlobal();
@@ -129,52 +109,44 @@ int module_init(char **status_info)
 	DBG("KaliDelimiters = %s\n", KaliDelimiters);
 	DBG("KaliExpandAbbreviations = %d\n", KaliExpandAbbreviations);
 
-	kali_message = NULL;
-
-	sem_init(&kali_semaphore, 0, 0);
-
-	DBG("Kali: creating new thread for kali_speak\n");
 	kali_speaking = 0;
-	ret = spd_pthread_create(&kali_speak_thread, NULL, _kali_speak, NULL);
-	if (ret != 0) {
-		DBG("Kali: thread failed\n");
-		*status_info =
-		    g_strdup("The module couldn't initialize threads "
-			     "This could be either an internal problem or an "
-			     "architecture problem. If you are sure your architecture "
-			     "supports threads, please report a bug.");
-		return -1;
-	}
+
+	buf = (char *)g_malloc((KaliMaxChunkLength + 1) * sizeof(char));
 
 	*status_info = g_strdup("Kali initialized successfully.");
 
 	return 0;
 }
 
-#undef ABORT
-
 SPDVoice **module_list_voices(void)
 {
 	return kali_voice_list;
 }
 
-int module_speak(const char * data, size_t bytes, SPDMessageType msgtype)
+void module_speak_sync(const char * data, size_t len, SPDMessageType msgtype)
 {
-	DBG("write()\n");
-
-	if (kali_speaking) {
-		DBG("Speaking when requested to write");
-		return 0;
-	}
-
 	DBG("Requested data: |%s|\n", data);
 
-	if (kali_message != NULL) {
-		g_free(kali_message);
-		kali_message = NULL;
+	if (kali_speaking) {
+		module_speak_error();
+		DBG("Speaking when requested to write");
+		return;
 	}
+
+	kali_speaking = 1;
+
+	AudioTrack track;
+#if defined(BYTE_ORDER) && (BYTE_ORDER == BIG_ENDIAN)
+	AudioFormat format = SPD_AUDIO_BE;
+#else
+	AudioFormat format = SPD_AUDIO_LE;
+#endif
+	const AudioTrackKali *wav;
+	char *kali_message;
+
 	kali_message = module_strip_ssml(data);
-	kali_message_type = SPD_MSGTYPE_TEXT;
+
+	module_speak_ok();
 
 	/* Setting voice */
 	UPDATE_PARAMETER(rate, kali_set_rate);
@@ -184,27 +156,92 @@ int module_speak(const char * data, size_t bytes, SPDMessageType msgtype)
 	UPDATE_STRING_PARAMETER(voice.name, kali_set_voice);
 	kali_set_voice(msg_settings.voice.name);
 
-	/* Send semaphore signal to the speaking thread */
-	kali_speaking = 1;
-	sem_post(&kali_semaphore);
+	unsigned int pos;
+	int bytes;
 
-	DBG("Kali: leaving write() normally\n\r");
-	return bytes;
+	DBG("kali: speaking thread starting.......\n");
+
+	pos = 0;
+	module_report_event_begin();
+	while (1) {
+		/* Process server events in case we were told to stop in between */
+		module_process(STDIN_FILENO, 0);
+
+		if (kali_pause_requested && (current_index_mark != -1)) {
+			DBG("Pause requested in parent, position %d\n",
+			    current_index_mark);
+			kali_pause_requested = 0;
+			kali_position = current_index_mark;
+			module_report_event_pause();
+			break;
+		}
+
+		if (kali_stop) {
+			DBG("Stop in child, terminating");
+			module_report_event_stop();
+			break;
+		}
+
+		bytes =
+		    module_get_message_part(kali_message, buf, &pos,
+					    KaliMaxChunkLength,
+					    KaliDelimiters);
+
+		if (bytes < 0) {
+			DBG("End of message");
+			module_report_event_end();
+			break;
+		}
+
+		if (bytes == 0) {
+			DBG("No data");
+			module_report_event_end();
+			break;
+		}
+
+		buf[bytes] = 0;
+		DBG("Returned %d bytes from get_part\n", bytes);
+		DBG("Text to synthesize is '%s'\n", buf);
+
+		DBG("Trying to synthesize text");
+		MessageKali((unsigned char *)buf);
+		while (QueryIndexKali() > 0)
+			;
+		wav =
+		    (const AudioTrackKali *)
+		    GetBufMultiKaliStd(0);
+
+		if (wav == NULL) {
+			DBG("Stop in child, terminating");
+			module_report_event_stop();
+			break;
+		}
+
+		track.num_samples = wav->num_samples;
+		track.num_channels = wav->num_channels;
+		track.sample_rate = wav->sample_rate;
+		track.bits = wav->bits;
+		track.samples = (signed short *)wav->samples;
+
+		DBG("Got %d samples", track.num_samples);
+		if (track.samples != NULL) {
+			DBG("Playing part of the message");
+			module_tts_output_server(&track, format);
+		}
+	}
+	g_free(kali_message);
+	kali_speaking = 0;
+	kali_stop = 0;
 }
 
 int module_stop(void)
 {
-	int ret;
 	DBG("kali: stop()\n");
 
+	if (kali_speaking)
+		kali_stop = 1;
+
 	kali_stop = 1;
-	if (module_audio_id) {
-		DBG("Stopping audio");
-		ret = spd_audio_stop(module_audio_id);
-		if (ret != 0)
-			DBG("WARNING: Non 0 value from spd_audio_stop: %d",
-			    ret);
-	}
 
 	return 0;
 }
@@ -238,149 +275,10 @@ int module_close(void)
 	quitteParle();
 	quitteGlobal();
 
-	DBG("Terminating threads");
-	if (module_terminate_thread(kali_speak_thread) != 0)
-		return -1;
-
-	sem_destroy(&kali_semaphore);
-
 	return 0;
 }
 
 /* Internal functions */
-
-void *_kali_speak(void *nothing)
-{
-	AudioTrack track;
-#if defined(BYTE_ORDER) && (BYTE_ORDER == BIG_ENDIAN)
-	AudioFormat format = SPD_AUDIO_BE;
-#else
-	AudioFormat format = SPD_AUDIO_LE;
-#endif
-	const AudioTrackKali *wav;
-	unsigned int pos;
-	char *buf;
-	int bytes;
-	int ret;
-
-	DBG("kali: speaking thread starting.......\n");
-
-	/* Make interruptible */
-	set_speaking_thread_parameters();
-
-	while (1) {
-		sem_wait(&kali_semaphore);
-		DBG("Semaphore on\n");
-
-		kali_stop = 0;
-		kali_speaking = 1;
-
-		/* TODO: free(buf) */
-		buf = (char *)g_malloc((KaliMaxChunkLength + 1) * sizeof(char));
-		pos = 0;
-		module_report_event_begin();
-		while (1) {
-			if (kali_stop) {
-				DBG("Stop in child, terminating");
-				kali_speaking = 0;
-				module_report_event_stop();
-				break;
-			}
-			bytes =
-			    module_get_message_part(kali_message, buf, &pos,
-						    KaliMaxChunkLength,
-						    KaliDelimiters);
-
-			if (bytes < 0) {
-				DBG("End of message");
-				kali_speaking = 0;
-				module_report_event_end();
-				break;
-			}
-
-			buf[bytes] = 0;
-			DBG("Returned %d bytes from get_part\n", bytes);
-			DBG("Text to synthesize is '%s'\n", buf);
-
-			if (kali_pause_requested && (current_index_mark != -1)) {
-				DBG("Pause requested in parent, position %d\n",
-				    current_index_mark);
-				kali_pause_requested = 0;
-				kali_position = current_index_mark;
-				break;
-			}
-
-			if (bytes > 0) {
-				DBG("Speaking in child...");
-
-				DBG("Trying to synthesize text");
-				MessageKali((unsigned char *)buf);
-				while (QueryIndexKali() > 0)
-					;
-				wav =
-				    (const AudioTrackKali *)
-				    GetBufMultiKaliStd(0);
-
-				if (wav == NULL) {
-					DBG("Stop in child, terminating");
-					kali_speaking = 0;
-					module_report_event_stop();
-					break;
-				}
-
-				track.num_samples = wav->num_samples;
-				track.num_channels = wav->num_channels;
-				track.sample_rate = wav->sample_rate;
-				track.bits = wav->bits;
-				track.samples = (signed short *)wav->samples;
-
-				DBG("Got %d samples", track.num_samples);
-				if (track.samples != NULL) {
-					if (kali_stop) {
-						DBG("Stop in child, terminating");
-						kali_speaking = 0;
-						module_report_event_stop();
-						break;
-					}
-					DBG("Playing part of the message");
-					ret = module_tts_output(track, format);
-					if (ret < 0)
-						DBG("ERROR: failed to play the track");
-					if (kali_stop) {
-						DBG("Stop in child, terminating (s)");
-						kali_speaking = 0;
-						module_report_event_stop();
-						break;
-					}
-				}
-			} else if (bytes == -1) {
-				DBG("End of data in speaking thread");
-				kali_speaking = 0;
-				module_report_event_end();
-				break;
-			} else {
-				kali_speaking = 0;
-				module_report_event_end();
-				break;
-			}
-
-			if (kali_stop) {
-				DBG("Stop in child, terminating");
-				kali_speaking = 0;
-				module_report_event_stop();
-				break;
-			}
-		}
-		kali_stop = 0;
-		g_free(buf);
-	}
-
-	kali_speaking = 0;
-
-	DBG("kali: speaking thread ended.......\n");
-
-	pthread_exit(NULL);
-}
 
 static void kali_set_rate(signed int rate)
 {
