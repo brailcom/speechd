eb4745737047038d9fefb14b86a0c5f9430e75e0
alberto tirla <albertotirla@gmail.com>
2024-09-17 20:08:34 +0300

    add more essential fields to the module_state structure
    * replace pw_main_loop with pw_thread_loop. This is required because the starting of a pipewire main loop blocks the starter thread untill it stops, which is not desirable for speech dispatcher
    * add a mechanism to facilitate sample transfer between speech dispatcher and pipewire. Pipewire includes a handy structure called spa_ringbuffer, which is a thread-safe ring buffer implementation. The use of atomics makes it possible to not require mutexes or other similar locks to guard buffer access

--- src/audio/pipewire.c
+++ src/audio/pipewire.c
@@ -3,25 +3,36 @@
 #endif
 
 #include <stdio.h>
-#include <pthread.h>
+
 #include <glib.h>
 
 #include <pipewire/pipewire.h>
-#include <spa/param/audio/format-utils.h>
+#include<pipewire/thread-loop.h>
 
+#include <spa/param/audio/format-utils.h>
+#include<spa/utils/ringbuffer.h>
+
+#include <spd_audio_plugin.h>
+#include "../common/common.h"
+
+//for the buffer size which will be allocated on the heap, to store the samples produced by speech dispatcher and read by pipewire
+//128  integers wide, this buffer will be used as backing storage by a ring buffer, which will also insure that the threads are syncronised when reading from and writing to the storage area
+#define SAMPLE_BUFFER_SIZE 128
+
+//speech dispatcher backend entry point, defined in multiple configurations
 #ifdef USE_DLOPEN
 #define SPD_AUDIO_PLUGIN_ENTRY spd_audio_plugin_get
 #else
 #define SPD_AUDIO_PLUGIN_ENTRY spd_pipewire_LTX_spd_audio_plugin_get
 #endif
-#include <spd_audio_plugin.h>
 
-#include "../common/common.h"
-
-
-struct module_state {
-        struct pw_main_loop *loop;
-        struct pw_stream *stream;
-        };
-
+//state of the backend, where all components are gathered. This will be allocated on the heap
+struct module_state
+{
+    AudioID id; //to comply with the speech dispatcher contract, make the first field of the struct the type expected by the spd callbacks, so that casting from it isn't entirely undefined behaviour
+    struct pw_thread_loop *loop; //a pipewire loop object which can be used without blocking the main thread, in this case the thread of speech dispatcher
+    struct pw_stream *stream; //this represents an instance of this module, a node of the media graph along with other metadata, which will be linked to the default output device
+    struct spa_ringbuffer *rb; //a thread-safe ring buffer implementation which uses atomic operations  to guarantee some semblance of thread safety, therefore it doesn't require a mutex
+char * sample_buffer; //the heap storage memory which will be backing the atomic ring buffer implementation, it's on the heap because this struct would be transfered across callback functions a lot, and such a large buffer could cause a stack overflow on some hardware architectures
+};
 
