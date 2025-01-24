a47dd288b069376c3563d1d122b78f2c70e45552
Sam Overton <github@samoverton.dev>
2025-01-24 17:30:54 +0000

    sd_piper: add license header to piper.c

--- src/modules/piper.c
+++ src/modules/piper.c
@@ -1,3 +1,26 @@
+
+/*
+ * piper.c - Speech Dispatcher backend for piper
+ *
+ * Copyright (C) 2025 Samuel Thibault <samuel.thibault@ens-lyon.org>
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * @author Sam Overton <github@samoverton.dev>
+ *
+ */
+
 #include <glib.h>
 #include <errno.h>
 #include <pthread.h>
